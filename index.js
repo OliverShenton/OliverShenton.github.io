ec29da4825b2881e5a2857710f90d5eea60e1dee
olive <olivermshenton@hotmail.com>
2024-07-21 17:46:40 +0100

    commit

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 'use strict';
+
+function employmentHideClick() {
+  let empHide = document.getElementById('employhidden');
+  if (empHide.style.display === 'none') {
+    empHide.style.display = 'block';
+  } else {
+    empHide.style.display = 'none';
+  }
+}
