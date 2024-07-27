593971bdde727b0b510a06a407066511b9d84631
olive <olivermshenton@hotmail.com>
2024-07-27 12:44:39 +0100

    commit

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 'use strict';
 
+// -------------------------------------------------------------------------------------------------------
+// Hide buttons
 function employmentHideClick() {
-  let empHide = document.getElementById('employhidden');
+  let empHide = document.getElementById('employmenthidden');
   if (empHide.style.display === 'none') {
     empHide.style.display = 'block';
   } else {
     empHide.style.display = 'none';
+  }
+}
+
+function educationHideClick() {
+  let eduHide = document.getElementById('educationhidden');
+  if (eduHide.style.display === 'none') {
+    eduHide.style.display = 'block';
+  } else {
+    eduHide.style.display = 'none';
   }
 }
