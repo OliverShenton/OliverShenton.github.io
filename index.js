d6efe88d9d93df0d5a0600403f652397ca35dd50
olive <olivermshenton@hotmail.com>
2024-08-08 12:56:02 +0100

    commit

--- index.js
+++ index.js
@@ -1,25 +1,4 @@
-'use strict';
+// 'use strict';
 
-// -------------------------------------------------------------------------------------------------------
-// Anchor Scrolling
-// Get the header element
-let header = document.querySelector('header');
-
-// Get the height of the header
-let headerHeight = header.offsetHeight;
-document.querySelectorAll('a[href^="#"]').forEach(function (anchor) {
-  anchor.addEventListener('click', function (event) {
-    event.preventDefault();
-
-    // Get the target element that
-    // the anchor link points to
-    let target = document.querySelector(this.getAttribute('href'));
-
-    let targetPosition = target.getBoundingClientRect().top - headerHeight;
-
-    window.scrollTo({
-      top: targetPosition + window.scrollY,
-      behavior: 'smooth',
-    });
-  });
-});
+// Change Availability colour
+document.getElementById('profile_availability').style.color = 'red';
